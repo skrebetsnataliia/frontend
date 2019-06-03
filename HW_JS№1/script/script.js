cc2008b15074dc328044cfaf30736de2dbc0e540
skrebetsnataliia <skrebetsnayasha98@gmail.com>
2019-06-03 12:19:23 +0300

    HomeWork JS

---
+++ HW_JS№1/script/script.js
@@ -0,0 +1,52 @@
+console.log("Task1");
+var name = "Natalia";
+var student;
+student = name;
+console.log(student);
+console.log(" ");
+console.log("Task2");
+var d = 11,
+    f = 2,
+    g = 36,
+    h = 3;
+console.log("11%2=", d % f, ",36%3=", g % h);
+var j = '5',
+    k = 2;
+console.log("'5'+2=", j + k);
+var z = "Hello",
+    x = 5;
+console.log("'Hello'+5=", z + x);
+var a = 1;
+console.log("a++", a++);
+console.log("a", a);
+console.log("++a", ++a);
+console.log(" ");
+console.log("Task3");
+var b = 3;
+a = 6;
+console.log("a>b", a > b);
+a = 3;
+console.log("a==b", a == b);
+b = 6;
+console.log("a!=b", a != b);
+a = 1;
+b = "";
+console.log("a===b", a === b);
+var A = "A",
+    B = "B",
+    Z = "Z";
+a = "a";
+console.log("'B'>'A'", B > A, "'a'>'Z'", a > Z);
+a = false;
+b = 0;
+console.log("a>=b", a >= b);
+console.log("('2'===2)?'ok':'not'", ('2' === 2) ? 'ok' : 'not');
+console.log(" ");
+console.log("Task4");
+a = 5;
+b = 3;
+console.log("(a>b)&&(a===b)", (a > b) && (a === b));
+console.log("true && 0 && ('a' < 'Z')", true && 0 && ('a' < 'Z'));
+console.log("(a > b) || true || ('2' == 2) || (false == '')", (a > b) || true || ('2' == 2) || (false == ''));
+console.log("(a < b) && (0 == false)", (a < b) && (0 == false));
+console.log("!(2 == 2) || (true && '')", !(2 == 2) || (true && ' '));
